b906fae0e0b1fd6dbd56f630dbde5f882443db9e
rasitdemirtas <rasitdmrts.58@gmail.com>
2021-09-14 03:38:10 +0300

    14 haziran ders sonu

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -15,7 +15,7 @@ namespace DegerVeReferansTipler
             // sayi1=?30
 
             int[] sayilar1 = new int[] { 10, 20, 30 };
-            int[] sayilar2 = new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };
             sayilar1 = sayilar2;
             sayilar2[0] = 999;
            // sayilar1[0]=? 999
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Raşit");
+            MyList<int> yas = new MyList<int>();
+            yas.Add(19);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
